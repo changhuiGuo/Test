bfd189ecee68d2e0e4ccd6115f15b32d2eb9c4a2
changhuiGuo <huichangguo@163.com>
2019-06-09 22:38:05 +0800

    添加test项目

---
+++ test/src/components/test.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="hello">
+    <a href class="third-party third-party-weixin">
+      <i></i>
+      <span></span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.third-party {
+  position: relative;
+  display: inline-block;
+  width: 48px;
+  height: 48px;
+  margin: 0 6%;
+  &:hover {
+    i {
+      &::after {
+        transform: scale(1);
+      }
+    }
+  }
+  span {
+    position: relative;
+    display: block;
+    width: 48px;
+    height: 48px;
+    background-size: 30px;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  i {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 48px;
+    height: 48px;
+    &::before {
+      content: "";
+      border-radius: 50%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+    &::after {
+      content: "";
+      transition: all 0.3s;
+      border-radius: 50%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      transform: scale(0);
+    }
+  }
+  &.third-party-weixin {
+    span {
+      background-image: url('https://b-gold-cdn.xitu.io/v3/static/img/wechat.63e1ce0.svg');
+    }
+    i {
+      &::before {
+        background-color: #20a839;
+      }
+      &::after {
+        background-color: #30cc54;
+      }
+    }
+  }
+}
+</style>
